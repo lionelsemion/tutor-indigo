7dee938d89451371792dccdc205ef3f8986280ab
lionelsemion <lionel@stuermer.ch>
2025-06-19 11:08:40 +0200

    c

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -122,7 +122,7 @@ for mfe in indigo_styled_mfes:
                 """
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.2.2'
-RUN npm install '@edx/brand@git+https://github.com/lionelsemion/brand-openedx.git#master'
+RUN npm install '@edx/brand@file:./../brand-openedx'
 
 """,
             ),
@@ -225,3 +225,4 @@ for mfe in indigo_styled_mfes:
   """,
         ),
     )
+    
